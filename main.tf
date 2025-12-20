0cb3e76fb1f16055731f616978a241b270354d83
ssl-cloud <sslcloudlearning@gmail.com>
2025-12-20 13:32:23 +0530

    main.tf modified:

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = var.location
+  location = var.location.us
 }
 
 resource "azurerm_virtual_network" "vnet" {
