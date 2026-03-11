23b2ce3b4af48e9cec759cbe1bf656cdce946731
ssl-cloud <sslcloudlearning@gmail.com>
2026-03-11 17:17:38 +0530

    JIRA-202-SUBNET-3 added

--- main.tf
+++ main.tf
@@ -52,6 +52,12 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+resource "azurerm_subnet" "subnet-3" {
+  name                 = "${var.prefix}-subnet-3"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.3.3.0/24"]
+}
 
 resource "azurerm_network_interface" "nic" {
   name                = "${var.prefix}-nic"
