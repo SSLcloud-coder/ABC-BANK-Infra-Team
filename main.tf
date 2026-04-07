10735db5fc5c994a7f7bc506f275f8e6ae19e789
ssl-cloud <sslcloudlearning@gmail.com>
2026-04-07 21:20:02 +0530

    Testing-team-2

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ resource "azurerm_virtual_network" "vnet" {
   location           = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["192.169.0.0/17"]
+  location           = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 resource "azurerm_virtual_network" "vnet-4" {
   name                = "${var.prefix}-vnet"
   address_space       = ["10.54.56.48/16"]
